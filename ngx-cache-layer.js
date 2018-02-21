a9c779aa97b48d160a3034ca0d78c0ff42d2a9f9
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2018-02-21 22:53:01 +0200

    chore(AMD): modules converted to AMD not ES2015

--- ngx-cache-layer.js
+++ ngx-cache-layer.js
@@ -1,4 +1,13 @@
 /**
  * Generated bundle index. Do not edit.
  */
-export { CACHE_MODULE_CONFIG, CACHE_MODULE_DI_CONFIG, CacheModule, CacheService, CacheLayer, CacheServiceConfigInterface } from './index';
+define(["require", "exports", "./index"], function (require, exports, index_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.CACHE_MODULE_CONFIG = index_1.CACHE_MODULE_CONFIG;
+    exports.CACHE_MODULE_DI_CONFIG = index_1.CACHE_MODULE_DI_CONFIG;
+    exports.CacheModule = index_1.CacheModule;
+    exports.CacheService = index_1.CacheService;
+    exports.CacheLayer = index_1.CacheLayer;
+    exports.CacheServiceConfigInterface = index_1.CacheServiceConfigInterface;
+});
